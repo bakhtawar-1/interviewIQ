a4ba3a7f6e1698c11e9a1e92b6a75993c9b6ffad
Rao Shahroze Ali <shahrozealirao@gmail.com>
2026-05-16 15:00:55 +0500

    fixed errors

--- intervieww/src/config.js
+++ intervieww/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "https://interviewiq-backend.koyeb.app";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+console.log("Connect to Backend at:", API_URL);
